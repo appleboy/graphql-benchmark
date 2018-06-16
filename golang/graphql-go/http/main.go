ba94528b140c6dc0c0f13dfe18e09e6081aa7385
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-06-17 07:52:15 +0800

    feat: init http default graphql

---
+++ golang/graphql-go/http/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/graphql-go/graphql"
+)
+
+// Schema
+var fields = graphql.Fields{
+	"hello": &graphql.Field{
+		Type: graphql.String,
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			return "world", nil
+		},
+	},
+}
+var rootQuery = graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+var schemaConfig = graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+var schema, _ = graphql.NewSchema(schemaConfig)
+
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		fmt.Printf("wrong result, unexpected errors: %v", result.Errors)
+	}
+	return result
+}
+
+func main() {
+
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		result := executeQuery(r.URL.Query().Get("query"), schema)
+		json.NewEncoder(w).Encode(result)
+	})
+
+	fmt.Println("Now server is running on port 8080")
+	fmt.Println("Test with Get      : curl -g 'http://localhost:8080/graphql?query={hello}'")
+	http.ListenAndServe(":8080", nil)
+
+}
